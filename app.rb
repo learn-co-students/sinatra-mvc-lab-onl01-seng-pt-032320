7fe18754943c709cd23ddf8434126943bdcc6ee0
Caroline <cvrieous@gmail.com>
2020-06-28 21:38:53 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
+# frozen_string_literal: true
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    piglatinizer = PigLatinizer.new()
+    @latinized_text = piglatinizer.piglatinize(params['user_phrase'])
+
+    erb :latinized
+  end
 end
