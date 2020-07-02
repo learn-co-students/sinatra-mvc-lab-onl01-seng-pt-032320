3cd1227f237b224057473ecbae890aca2a6a027b
Bryce B <bryce.boddy@gmail.com>
2020-07-02 08:22:00 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,13 +1,19 @@
 class PigLatinizer
   attr_reader :text
+  
+  def piglatinize(sentence)
+    array = sentence.split(" ")
+    array.map { |word| piglatinizer(word)}.join(" ")
+  end
 
-  def piglatinize(word)
+  def piglatinizer(word)
     if word.start_with?("a", "e", "i", "o", "u", "y")
-      word = "#{word}way"
-      word
+      "#{word}way"
     else
-      word = word.split
-      pig_lat_word = "#{word.last(word.count).join}ay"
+      word = word.split(//) #turns our word into an array
+      word = word.drop(1) #removes the first letter of the word
+      word = word.join #converts the word back to a string
+      "#{word}ay" #adds the pig latin to the word and returns it
     end
   end
 end
